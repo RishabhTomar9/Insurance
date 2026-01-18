629a3ebf225723c6b707d4d25712ef6deda599d1
Rishabh tomar <rishabhtomar9999@gmail.com>
2026-01-18 15:14:46 +0530

    all old removed

--- frontend-new/src/App.jsx
+++ frontend-new/src/App.jsx
@@ -1,27 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import VehiclesPage from './pages/VehiclesPage';
-import OwnersPage from './pages/OwnersPage';
-import PoliciesPage from './pages/PoliciesPage';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <div className="bg-gray-100 min-h-screen">
-        <Navbar />
-        <main className="container mx-auto p-4">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/vehicles" element={<VehiclesPage />} />
-            <Route path="/owners" element={<OwnersPage />} />
-            <Route path="/policies" element={<PoliciesPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
